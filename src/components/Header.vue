80351bef7bd358c645658b51799a1eea37ee700c
Azamat Iskanderov <IskanderovAk98@gmail.com>
2023-09-14 13:28:22 +0600

    add header and router

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import IconFavorites from '../assets/icons/IconFavorites.vue';
+import IconSearch from '../assets/icons/IconSearch.vue';
+</script>
+
+<template>
+ <header>
+  <div class="container">
+    <div class="header_content">
+      <RouterLink to="/">
+        <img class="logo" src="./assets/icons/logo_art.svg" />
+      </RouterLink>
+      <nav>
+        <RouterLink class="nav_link" to="/">
+          <i>
+            <IconSearch />
+          </i>
+          <span>Поиск</span>
+        </RouterLink>
+        <RouterLink class="nav_link" to="/favorites">
+            <i>
+              <IconFavorites />
+            </i>
+            <span>Избранное</span>
+          </RouterLink>
+      </nav>
+    </div>
+  </div>
+ </header>
+</template>
+
+<style scoped>
+</style>
